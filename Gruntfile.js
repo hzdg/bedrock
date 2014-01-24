d2de58de6be8a5f7b6dbf81de6a1c4e4a8315ab7
Ryan Bagwell <ryan@ryanbagwell.com>
2014-01-24 15:10:11 -0600

    adding grunt task to compile all mixin files into a single less file to styledocco will create a single file with references to all mixins

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,33 @@ module.exports = function (grunt) {
                 commitFiles: ['-a'],
                 push: false
             }
+        },
+        concat: {
+            options: {
+
+            },
+            dist: {
+                src: ['mixins/*.less', '!mixins/all.less'],
+                dest: 'mixins/all.less'
+            }
+
+
+        },
+        styledocco: {
+            dist: {
+                options: {
+                    name: 'Bedrock'
+                },
+                files: {
+                    'docs': ['mixins/all.less']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-styledocco');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('build', ['concat', 'styledocco']);
 };
